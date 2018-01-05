d21315745493f4b31727b13fd6b81f52955b0e2b
James Briggs <james@bzode.com>
2018-01-04 20:14:21 -0700

    Reverted back to regular rails app.

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module Champ
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.api_only = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
